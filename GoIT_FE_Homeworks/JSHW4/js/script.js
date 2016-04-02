2839dceb9bc161a7035ec4834255ad9b47cb523c
Sergey Kulinich <sparkof@gmail.com>
2016-04-02 18:05:50 +0300

    fixed 4th jshw

--- GoIT_FE_Homeworks/JSHW4/js/script.js
+++ GoIT_FE_Homeworks/JSHW4/js/script.js
@@ -22,20 +22,25 @@ $(function () {
 
 (function tooltips() {
   $("input[type='text']").after("<em></em>");
+  var animation = false
 
   function showTooltip() {
-    var hoverText = $(this).attr("title");
-    var $startPositionLeft = $(this).position().left + $(this).width();
-
     $(this).data('title',$(this).attr('title'));
     $(this).removeAttr('title');
+
+    var hoverText = $(this).data("title");
+    var $startPositionLeft = $(this).position().left + $(this).width();
+
     $(this).siblings("em").text(hoverText);
     $(this).siblings("em").css({left: $startPositionLeft});
-    $(this).siblings("em").animate({opacity: "show", left: "+=35"}, "normal");
+    $(this).siblings("em").animate({opacity: 1, left: "+=35"}, "normal");
   }
   function hideTooltip() {
-    $(this).siblings("em").animate({opacity: "hide"}, "fast");
+    $(this).siblings("em").animate({opacity: 0, left: "-=35"}, "fast");
   }
   $("input[type='text']").hover(showTooltip, hideTooltip);
+  $('button').click(function () {
+    $("input[type='text']").each(showTooltip);
+  })
 })();
 });
