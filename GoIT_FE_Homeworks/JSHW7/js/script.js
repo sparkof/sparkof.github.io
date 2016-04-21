b3984ce5d07cffa0fb632deb075d39964bedcdf2
sparkof <sparkof@gmail.com>
2016-04-22 02:32:19 +0300

    Added files via upload

--- GoIT_FE_Homeworks/JSHW7/js/script.js
+++ GoIT_FE_Homeworks/JSHW7/js/script.js
@@ -9,38 +9,42 @@ $(function () {
   });
   $('body').append(content);
 
-  var result;
-
   $('.questionWrapper :submit').on('click', function (e) {
     e.preventDefault();
     checkResult();
   });
 
   function checkResult() {
-    result = 0;
-    var totalCorrectAnswers = 0;
-
+    var result = 0;
+    // var totalCorrectAnswers = 0;
     var $questionList = $('.questionWrapper ol>li');
     $questionList.each(function (index) {
-      debugger;
+          debugger;
+      var currentQuestionCorrectAnswers = 0;
+      var checkedAnswers = 0;
+      var checkedCorrectAnswers = 0;
+
       for ( var i = 0; i < questions.length; i++) {
         if ( $( this ).html().indexOf(questions[i].text) + 1 ) {       //поиск нужного вопроса
+          checkedAnswers = $( this ).find(':checked + label').length;
 
           for (var j = 0; j < questions[i].answers.length; j++) {      //суммарный подсчёт правильных ответов
             if (questions[i].answers[j].correct === true) {
-              totalCorrectAnswers +=1;
+              currentQuestionCorrectAnswers += 1;
+              // totalCorrectAnswers +=1;
             }
             $( this ).find(':checked + label').each(function (index2) {
                 if ( ($( this ).html().indexOf(questions[i].answers[j].text) + 1) && questions[i].answers[j].correct === true ) {
-                  result +=1;
+                  checkedCorrectAnswers +=1;
                 };
               });
           };
         };
       };
+      result += checkedCorrectAnswers / checkedAnswers / currentQuestionCorrectAnswers;
     });
 
-    console.log('Ваш результат:' + (result/totalCorrectAnswers * 100) + '% ответов правильные');
-
+    console.log('Ваш результат:' + (result / $questionList.length * 100) + '% правильных ответов');
+    // return result / questionList.length * 100;
   };
 });
