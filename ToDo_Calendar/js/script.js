84c001f8f3b31ebc84a599224a1665adbfb3461f
sparkof <sparkof@gmail.com>
2016-06-17 17:15:52 +0300

    todo calendar 2

--- ToDo_Calendar/js/script.js
+++ ToDo_Calendar/js/script.js
@@ -1,4 +1,5 @@
 var displayedMonth = new Date().getMonth();
+var displayedYears = new Date().getFullYear();
 
 function changeDisplayedMonth(a) {
   if (a === 'next') {
@@ -15,4 +16,50 @@ function changeDisplayedMonth(a) {
     };
   }
   return displayedMonth;
+}
+
+function getMonthName(month) {
+  var monthName;
+  switch (month) {
+    case 0:
+      monthName = 'январь';
+      break;
+    case 1:
+      monthName = 'февраль';
+      break;
+    case 2:
+      monthName = 'март';
+      break;
+    case 3:
+      monthName = 'апрель';
+      break;
+    case 4:
+      monthName = 'май';
+      break;
+    case 5:
+      monthName = 'июнь';
+      break;
+    case 6:
+      monthName = 'июль';
+      break;
+    case 7:
+      monthName = 'август';
+      break;
+    case 8:
+      monthName = 'сентябрь';
+      break;
+    case 9:
+      monthName = 'октябрь';
+      break;
+    case 10:
+      monthName = 'ноябрь';
+      break;
+    case 11:
+      monthName = 'декабрь';
+      break;
+    default:
+      console.log('error of monthName transormation');
+
+  }
+  return monthName;
 }
